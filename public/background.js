3738241995cefacebc1ad68f32e3a88a688b6130
dridiha98@gmail.com <dridiha98@gmail.com>
2022-07-11 20:51:16 +0200

    final version

--- public/background.js
+++ public/background.js
@@ -1,22 +1,3 @@
-chrome.runtime.onMessage.addListener((req) => {
-    console.log(req.message)
-    
-    
-    if (req.message == 'openPopup'){    
-           
-        
-        const popupUrl = chrome.runtime.getURL('index.html')
-        chrome.windows.create({
-            url: popupUrl,
-            type: "popup",
-            top: req.positionY,
-            left: req.positionX,
-            height : 680, 
-            width : 550
-        })
-        
-    }
-})
 // chrome.runtime.onMessage.addListener((req) => {
 //     console.log(req.message)
     
@@ -25,32 +6,53 @@ chrome.runtime.onMessage.addListener((req) => {
            
         
 //         const popupUrl = chrome.runtime.getURL('index.html')
-//         chrome.tabs.query({url: popupUrl}, (tabs) => {
-//             console.log(tabs[0])
-//             if(tabs.length == 0){
-//                 chrome.windows.create({
-//                     url: "popup.html", 
-//                     type: "popup",
-//                     height : 580, 
-//                     width : 450, 
-//                     top: req.positionY,
-//                     left: req.positionX
+//         chrome.windows.create({
+//             url: popupUrl,
+//             type: "popup",
+//             top: req.positionY,
+//             left: req.positionX,
+//             height : 680, 
+//             width : 550
+//         })
+        
+//     }
+// })
+chrome.runtime.onMessage.addListener((req) => {
+    
+    
+    
+    if (req.message == 'openPopup'){    
+           
+        
+        const popupUrl = chrome.runtime.getURL('index.html')
+        chrome.tabs.query({url: popupUrl}, (tabs) => {
+            console.log(tabs[0])
+            if(tabs.length == 0){
+                chrome.windows.create({
+                    url: "popup.html", 
+                    type: "popup",
+                    height : 580, 
+                    width : 450, 
+                    top: req.positionY,
+                    left: req.positionX
                     
-//                 })
+                })
 
-//             } else chrome.tabs.get(tabs[0].id, (tab)=>{
-//                 chrome.windows.get(tab.windowId, (win)=>{
-//                     console.log(win)
-//                     chrome.windows.update(win.id, {drawAttention: true, focused: true})
-//                 })
-//             })
+            } else chrome.tabs.get(tabs[0].id, (tab)=>{
+                chrome.windows.get(tab.windowId, (win)=>{
+                    console.log(win)
+                    chrome.windows.update(win.id, {drawAttention: true, focused: true})
+                })
+            })
             
             
-//            // chrome.windows.update(tabs[0].windowId, {drawAttention: true})
+           // chrome.windows.update(tabs[0].windowId, {drawAttention: true})
                   
             
 
-//         })
+        })
+    }
+})
             
         
 //        /* chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
